8b9f76aaa46e7b95252c56e2fcdc56f2cdec9aec
alex <ate001@ucr.edu>
2019-03-04 21:39:54 -0800

    Fixed small bugs. Look at dev log

--- blackjack.py
+++ blackjack.py
@@ -7,9 +7,19 @@ import numpy
 #	potential problems to fix: in results, i am only taking care of a 2 way draw for now.
 #	Need to fix results.
 #	Tie is not working, and also need to account for 2+ way tie
-#	and an emply sum (means player bust)
+#	and an empty sum (means player bust)
 #		- could just delete that player from the results calculation
 #			-need to make sure they are not deleted when restarting
+#
+#
+#
+#	3/4/19
+#	Ran into a bug when a player has 2 aces.
+#	Might be fixed when we set aceFlag = Flase in sumCards
+#
+#	As of 9:23 pm. I think i fixed 2+ way tie and the sum with >= 2 aces
+#	As of 9:38... NEED TO FIX multiple aces still. 
+#
 ####################################################################################################
 
 
@@ -105,6 +115,7 @@ class game:
 			tmpSum = 0
 			aceCard = False
 			playerSum = []
+			aceCounter = 0
 			for j in range(len(self.gameBoard[i])):
 
 				currCard = self.gameBoard[i][j][0];
@@ -112,17 +123,21 @@ class game:
 				if currCard == "J" or currCard == "Q" or currCard == "K":
 					tmpSum += 10
 				elif currCard == "A":
-					aceCard = True
+					# aceCard = True
+					aceCounter += 1
 				else:
 					tmpSum += currCard
 
-			if aceCard:
+			# if aceCard:
+			while aceCounter > 0:
+				# aceCard = False
 				tmpSum += 1
 				if tmpSum <= 21:
 					playerSum.append(tmpSum)
 				tmpSum += 10
 				if tmpSum <= 21:
 					playerSum.append(tmpSum)
+				aceCounter -= 1
 			else:
 				if tmpSum <= 21:
 					playerSum.append(tmpSum)
@@ -132,12 +147,18 @@ class game:
 
 		#what is displayed to the terminal for players to see.
 		for i in range(len(playersList)):
-			print "------------------"
+			print "--------------------------------------"
 			if 21 in self.sumList[i]:
-				print playersList[i]
-				print self.gameBoard[i]
-				print "Blackjack!!!"
+				if len(self.sumList[i]) == 2:
+					print playersList[i]
+					print self.gameBoard[i]
+					print "Blackjack!!!"
+				else:
+					print playersList[i]
+					print self.gameBoard[i]
+					print "21!!!"
 			elif len(self.sumList[i]) == 0:
+				print playersList[i]
 				print self.gameBoard[i]
 				print "Bust!!!"
 				self.bustList[i] = True
@@ -145,15 +166,16 @@ class game:
 				print playersList[i]
 				print self.gameBoard[i]
 				print self.sumList[i]
-		print "------------------"
+		print "--------------------------------------"
 			
 	def results(self, playersList):
+
 		#want to sort it so removing the smallest sum is best
 		for i in range(len(self.sumList)):
-			self.sumList[i].sort()
 			if len(self.sumList[i]) == 1:
 				continue
 			elif len(self.sumList[i]) != 0:
+				self.sumList[i].sort()
 				tmp = self.sumList[i][-1]
 				self.sumList[i] = []
 				self.sumList[i].append(tmp)
@@ -165,25 +187,42 @@ class game:
 		secondDraw = -1
 
 		for i in range(len(self.sumList)):
+			if len(self.sumList[i]) == 0:
+				continue
 			#need sumList[i][0] because we are checking a list, and need to check
 			#its number in the list, hence the [0]
 			#save the winner index so we can just say winner is at
 			#playerList[winnerIndex] or something like that
 			if self.sumList[i][0] > highest:
+				# print "Highest Changed. New highest is: ", self.sumList[i][0], "by", playersList[i] 
 				highest = self.sumList[i][0]
 				winnerIndex = i
 				draw = False
 			elif self.sumList[i][0] == highest:
+				# print "Draw Detected."
+				# print "Previous Highest = ", highest, "Which is", playersList[winnerIndex], "'s card"
+				# print "Now there is a tie with", playersList[i], "'s card"
 				#could be a problem if there are more than 2 draws...
 				draw = True
 				firstDraw = winnerIndex
 				secondDraw = i
-				print "asd"
+				# print "asd"
 
-		if draw == False:
-			print "Tie between: ", playersList[firstDraw], playersList[secondDraw] 
-		else:
+		if draw == True:
+			# print "Tie between: ", playersList[firstDraw], playersList[secondDraw]
+			print "Tie between: "
+			#done so we can iterate through and print out whoever has the same highest
+			for i in range(len(self.sumList)):
+				if len(self.sumList[i]) == 0:
+					continue
+				if self.sumList[i][0] == highest:
+					print playersList[i]
+		elif False in self.bustList:
+			print self.bustList
 			print "Winner is: ", playersList[winnerIndex]
+		else:
+			# print "Winner is: ", playersList[winnerIndex]
+			print "No one won."
 
 
 def main():
